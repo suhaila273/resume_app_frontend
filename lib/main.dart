dd1b123d6833d88a41951535d877a2db374e3e3e
suhaila273 <suhailasalam273@gmail.com>
2024-02-11 17:16:35 +0530

    new project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(HomePage());
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
